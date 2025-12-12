3a31cdb826b63329878c853c02e6d7471f4b2551
Naeem Gitonga <jahagitonga@Jahas-MacBook-Pro.local>
2025-12-12 19:09:04 +0000

    chore: update readme

--- http/README.md
+++ http/README.md
@@ -1,13 +1,15 @@
 ## to run
 create .venv:
-python -m venv .venv
+`python -m venv .venv`
 
 activated it using:
-source .venv/bin/activate
+`source .venv/bin/activate`
 
+install packages:
+`pip install -r requirements.txt`
 
 start app:
-python stateserver.py
+`python stateserver.py`
 
 deactivate it using the following when you are done:
 `deactivate`
--- terraform.tf
+++ terraform.tf
@@ -24,21 +24,21 @@ terraform {
   
   }
 
-  backend "http" {
-    address        = "http://localhost:5001/terraform_state/4cdd0c76-d78b-11e9-9bea-db9cd8374f3a"
-    lock_address   = "http://localhost:5001/terraform_lock/4cdd0c76-d78b-11e9-9bea-db9cd8374f3a"
-    lock_method    = "PUT"
-    unlock_address = "http://localhost:5001/terraform_lock/4cdd0c76-d78b-11e9-9bea-db9cd8374f3a"
-    unlock_method  = "DELETE"
-  }
-  
-  # backend "s3" {
-  #   bucket = "ng-tf-lab-backend"
-  #   region = "us-east-1"
-  #   key    = "terraform.tfstate"
-  #   dynamodb_table = "terraform-locks"
-  #   encrypt = true
+  # backend "http" {
+  #   address        = "http://localhost:5001/terraform_state/4cdd0c76-d78b-11e9-9bea-db9cd8374f3a"
+  #   lock_address   = "http://localhost:5001/terraform_lock/4cdd0c76-d78b-11e9-9bea-db9cd8374f3a"
+  #   lock_method    = "PUT"
+  #   unlock_address = "http://localhost:5001/terraform_lock/4cdd0c76-d78b-11e9-9bea-db9cd8374f3a"
+  #   unlock_method  = "DELETE"
   # }
+  
+  backend "s3" {
+    bucket = "ng-tf-lab-backend"
+    region = "us-east-1"
+    key    = "terraform.tfstate"
+    dynamodb_table = "terraform-locks"
+    encrypt = true
+  }
 
   # backend "remote" {
   #   hostname     = "app.terraform.io"
